f9f82edb3f103e1f29efc81c70399d55be601a89
danielkestrelnew <europaprotocol@gmail.com>
2025-12-09 14:53:46 +0100

    update code

--- average_speed/src/main.rs
+++ average_speed/src/main.rs
@@ -1,7 +1,9 @@
-mod ave_speed;
-
-use ave_speed::average_speed;
+use average_speed::ave_speed::average_speed;
 fn main() {
 
-    println!("{}", average_speed(30.0, 60.0));
+    let speed1 = 30.0;
+    let speed2 = 60.0;
+
+    println!("The average speed of {} and {} is: ", speed1, speed2);
+    println!("{}", average_speed(speed1, speed2));
 }
