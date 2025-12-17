86069294e5b2164a580c3bee4d99d1171f5520dc
danielkestrelnew <europaprotocol@gmail.com>
2025-12-17 01:01:50 +0100

    refactor code

--- kepler_3rd_law/src/main.rs
+++ kepler_3rd_law/src/main.rs
@@ -1,17 +1,14 @@
 use std::f64::consts::PI;
 
-fn calculate_planet_distance_from_sun(period: f64) -> f64 {
+fn calculate_planet_distance_from_sun(period_seconds: f64) -> f64 {
     
     let mass_of_sun: f64 = 1.99e30;
     let gravity_constant: f64 = 6.67e-11;
-    let period_sqr = period * period;
     
-    let numerator = gravity_constant * mass_of_sun * period_sqr;
+    let numerator = gravity_constant * mass_of_sun * period_seconds.powi(2);
     let denominator = 4.0 * PI * PI;
     
-    let n = 1.0/3.0;
-    
-    let distance = (numerator / denominator).powf(n);
+    let distance = (numerator / denominator).powf(1.0/3.0);
     
     distance
 }
